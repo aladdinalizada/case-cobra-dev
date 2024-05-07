15488994dafd53aad14ec9ebb14f74378acd174b
Aladdin Alizada <tu7c5zneb@code.edu.az>
2024-05-08 00:49:03 +0400

    inital commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:pt-24 lg:pb-52 lg:grid-cols-3 lg:gap-x-0 xl:gap-x-8 xl:pt-32 sm:pb-32">
+          <div></div>
+        </MaxWidthWrapper>
+      </section>
+    </div>
+  );
+}
