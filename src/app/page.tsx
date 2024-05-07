b32a56f1059dabae8b4afadec5799905ec874e60
Aladdin Alizada <tu7c5zneb@code.edu.az>
2024-05-08 01:49:49 +0400

    complete: Navbar and Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,122 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-
+import Phone from "@/components/Phone";
+// Import Images and Icons
+import Image from "next/image";
+import { Check, Star } from "lucide-react";
+// Line
+import Line from "../../public/line.png";
+// User Image
+import YourImage from "../../public/your-image.png";
+// Snake Image
+import Snake1 from "../../public/snake-1.png";
+// Users Images
+import User1 from "../../public/users/user-1.png";
+import User2 from "../../public/users/user-2.png";
+import User3 from "../../public/users/user-3.png";
+import User4 from "../../public/users/user-4.jpg";
+import User5 from "../../public/users/user-5.jpg";
+////////////////////////////////////////////////////////////////////////////////////////
 export default function Home() {
   return (
     <div className="bg-slate-50">
       <section>
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:pt-24 lg:pb-52 lg:grid-cols-3 lg:gap-x-0 xl:gap-x-8 xl:pt-32 sm:pb-32">
-          <div></div>
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28" />
+                <Image src={Snake1} className="w-full" alt={""} />
+              </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Your Image on a{" "}
+                <span className="bg-green-600 px-2 text-white">Custom</span>{" "}
+                Phone Case
+              </h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Capture your favorite memories with your own,{" "}
+                <span className="font-semibold">one-of-one</span> phone case.
+                CaseCobra allows you to protect your memories, not just your
+                phone case.
+              </p>
+
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    High-quality, durable material
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />5 year
+                    print guarantee
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    Modern iPhone models supported
+                  </li>
+                </div>
+              </ul>
+
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
+                <div className="flex -space-x-4">
+                  <Image
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src={User1}
+                    alt="user image 1"
+                  />
+                  <Image
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src={User2}
+                    alt="user image 2"
+                  />
+                  <Image
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src={User3}
+                    alt="user image 3"
+                  />
+                  <Image
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src={User4}
+                    alt="user image 4"
+                  />
+                  <Image
+                    className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src={User5}
+                    alt="user image 5"
+                  />
+                </div>
+
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                  </div>
+
+                  <p>
+                    <span className="font-semibold">1.250</span> happy customers
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
+              <Image
+                src={YourImage}
+                className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
+                alt={""}
+              />
+              <Image
+                src={Line}
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+                alt={""}
+              />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
+            </div>
+          </div>
         </MaxWidthWrapper>
       </section>
     </div>
